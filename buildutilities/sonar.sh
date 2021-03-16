c7b176ffa00352141e164786d8499017dc4bf7b2
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-17 10:00:33 +1030

    Update sonar.sh

--- buildutilities/sonar.sh
+++ buildutilities/sonar.sh
@@ -42,7 +42,7 @@ if [ ! -z $2 ]; then
   else
     echo "This is a Pull Request"
     SONAR_PULLREQUEST_BRANCH="$(echo $1 | awk '{split($0,a,"/"); print a[1]}')/$4"
-    sonar-scanner -X \
+    sonar-scanner \
       -Dproject.settings=/home/runner/work/constellation/constellation/sonar-project.properties \
       -Dsonar.login=$5 \
       -Dsonar.projectBaseDir=/home/runner/work/constellation/constellation/

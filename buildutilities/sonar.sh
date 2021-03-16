bd7beb58964eb3657d9460def8f8b2d81f845f98
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-16 12:57:57 +1030

    test script

--- buildutilities/sonar.sh
+++ buildutilities/sonar.sh
@@ -4,6 +4,27 @@ source buildutilities/functions.sh
 
 title "Run Sonar Scanning"
 
+cd /tmp || exit
+echo "Downloading sonar-scanner....."
+if [ -d "/tmp/sonar-scanner-cli-3.2.0.1227-linux.zip" ];then
+    sudo rm /tmp/sonar-scanner-cli-3.2.0.1227-linux.zip
+fi
+wget -q https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227-linux.zip
+echo "Download completed."
+
+echo "Unziping downloaded file..."
+unzip sonar-scanner-cli-3.2.0.1227-linux.zip
+echo "Unzip completed."
+rm sonar-scanner-cli-3.2.0.1227-linux.zip
+
+echo "Installing to opt..."
+if [ -d "/var/opt/sonar-scanner-3.2.0.1227-linux" ];then
+    sudo rm -rf /var/opt/sonar-scanner-3.2.0.1227-linux
+fi
+sudo mv sonar-scanner-3.2.0.1227-linux /var/opt
+
+echo "Installation completed successfully."
+
 if [ ! -z $2 ]; then
   if [ $1 != "aldebaran30701/constellation" ]; then
     echo "skipping running sonar-scanner"

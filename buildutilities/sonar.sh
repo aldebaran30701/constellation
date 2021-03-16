121e7f62bd87f9598514d9691d66a8b24b930734
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-17 08:08:38 +1030

    Update sonar.sh

--- buildutilities/sonar.sh
+++ buildutilities/sonar.sh
@@ -36,19 +36,8 @@ if [ ! -z $2 ]; then
     echo "This is a Pull Request"
     SONAR_PULLREQUEST_BRANCH="$(echo $1 | awk '{split($0,a,"/"); print a[1]}')/$4"
     sonar-scanner \
+      -Dproject.settings=/home/runner/work/constellation/constellation/sonar-project.properties
       -Dsonar.login=$5 \
-      -Dsonar.projectKey=aldebaran30701_constellation \
-      -Dsonar.organization=aldebaran30701 \
-      -Dsonar.scm.provider=git \
-      -Dsonar.pullrequest.github.repository=aldebaran30701/constellation \
-      -Dsonar.pullrequest.provider=github \
-      -Dsonar.pullrequest.github.endpoint=https://api.github.com/ \
-      -Dsonar.projectBaseDir=./ \
-      -Dsonar.modules=ProjectUpdater,CoreAlgorithmPlugins,CoreAnalyticSchema,CoreAnalyticView,CoreArrangementPlugins,CoreAttributeEditorView,CoreConversationView,CoreDataAccessView,CoreDependencies,CoreFindView,CoreFunctionality,CoreGraphFile,CoreGraphFramework,CoreGraphNode,CoreGraphUtilities,CoreHistogramView,CoreImportExportPlugins,CoreInteractiveGraph,CoreLayersView,CoreMapView,CoreNamedSelectionView,CoreOpenGLDisplay,CorePluginFramework,CorePluginReporterView,CorePreferences,CoreQualityControlView,CoreScatterPlotView,CoreSchemaView,CoreScriptingView,CoreSecurity,CoreTableView,CoreTesting,CoreTimelineView,CoreUtilities,CoreViewFramework,CoreVisualGraph,CoreVisualSchema,CoreWebServer,CoreWebView,CoreWelcomeView \
-      -Dsonar.sources=./src \
-      -Dsonar.host.url=http://sonarcloud.io \
-      -Dsonar.java.binaries=. \
-      -Dsonar.java.libraries=../CoreDependencies/ \
       -Dsonar.pullrequest.key=$2 \
       -Dsonar.pullrequest.branch="${SONAR_PULLREQUEST_BRANCH}" \
       -Dsonar.pullrequest.base=$3

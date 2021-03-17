4878a4329307692f28c44accf2273b40d1d773dd
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-18 09:10:02 +1030

    Revert "Update update-dependencies.sh"
    This reverts commit e0dad7b5ea1d0ac564a2f62c58b19df40f2191e9.

--- githubutilities/update-dependencies.sh
+++ githubutilities/update-dependencies.sh
@@ -8,9 +8,13 @@ echo $PWD
 sudo apt update
 sudo apt-get install tree
 echo "before updatedeps"
-tree -a ~/
+tree -a /root
 ant \
+  -Dnbplatform.active.dir=/usr/local/netbeans \
+  -Dnbplatform.default.netbeans.dest.dir=/usr/local/netbeans \
+  -Dnbplatform.default.harness.dir=/usr/local/netbeans/harness \
   -Dupdate.dependencies=true \
+  -Dbuild.compiler.debug=true update-dependencies-clean-build
 
 echo "after updatedeps"
-tree -a ~/
+tree -a /root

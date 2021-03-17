e0dad7b5ea1d0ac564a2f62c58b19df40f2191e9
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-18 09:04:02 +1030

    Update update-dependencies.sh

--- githubutilities/update-dependencies.sh
+++ githubutilities/update-dependencies.sh
@@ -8,13 +8,9 @@ echo $PWD
 sudo apt update
 sudo apt-get install tree
 echo "before updatedeps"
-tree -a /root
+tree -a ~/
 ant \
-  -Dnbplatform.active.dir=/usr/local/netbeans \
-  -Dnbplatform.default.netbeans.dest.dir=/usr/local/netbeans \
-  -Dnbplatform.default.harness.dir=/usr/local/netbeans/harness \
   -Dupdate.dependencies=true \
-  -Dbuild.compiler.debug=true update-dependencies-clean-build
 
 echo "after updatedeps"
-tree -a /root
+tree -a ~/

696ad0b34bd83c6497f9d3165e5519a0d1b4a09c
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-19 09:29:27 +1030

    Revert "docker adding install"
    This reverts commit a0ceb4077385947cd60481cd6dfed17d14c14725.

--- githubutilities/sonar.sh
+++ githubutilities/sonar.sh
@@ -15,6 +15,27 @@ echo "Finished Updating dependencies and building."
 
 title "Run Sonar Scanning"
 
+cd /tmp || exit
+echo "Downloading sonar-scanner....."
+if [ -d "/tmp/sonar-scanner-cli-3.2.0.1227-linux.zip" ];then
+    sudo rm /tmp/sonar-scanner-cli-3.2.0.1227-linux.zip
+fi
+wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
+echo "Download completed."
+
+echo "Unziping downloaded file..."
+unzip sonar-scanner-cli-4.0.0.1744-linux.zip
+echo "Unzip completed."
+rm sonar-scanner-cli-4.0.0.1744-linux.zip
+
+echo "Installing to opt..."
+if [ -d "/var/opt/sonar-scanner-4.0.0.1744-linux" ];then
+    rm -rf /var/opt/sonar-scanner-4.0.0.1744-linux
+fi
+mv sonar-scanner-4.0.0.1744-linux /var/opt/sonar-scanner
+export PATH="$PATH:/var/opt/sonar-scanner/bin"
+echo "Installation completed successfully."
+
 if [ ! -z $2 ]; then
   if [ $1 != "aldebaran30701/constellation" ]; then
     echo "skipping running sonar-scanner"
